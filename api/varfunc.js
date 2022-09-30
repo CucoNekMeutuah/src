2aef92b95d819882ab18ee800f822c695008c62e
Cuco Nek Meutuah <masri18ac@gmail.com>
2022-09-30 21:18:56 +0700

    Create varfunc.js

---
+++ api/varfunc.js
@@ -0,0 +1,15 @@
+        var g3tApi = ["DonPay", "https://bo.dipayin.my.id/dev/public/v2/api/", "https://bo.dipayin.my.id/dev/public/v2/detail/", "073809c85bbee275e8e77b7969799bde34244ef537dc514252f7d6c3229e7d9c"];
+        
+        var cp = ["https://i.ibb.co/BCbGrkm/20220624-035810.png"];
+        
+        var nama2 = ["DANA", "OVO", "LinkAja", "Grab", "GoPay", "ShopeePay", "iSAKU"];
+        
+        
+        var dana = ["dana-304037", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-9p6xMnSxAKgy71KAacDVO2b5D7IeaaSziA&usqp=CAU"];
+        var ovo = ["ovo-304038", "https://i.ibb.co/TTHhhNG/20220624-025928.png"];
+        var linkaja = ["linkaja-345950", "https://i.ibb.co/HXy9LmG/images-1.png"];
+        var grab = ["grab-347162", "https://i.ibb.co/RQHcnxn/images-2.png"];
+        var gopay = ["gopay-304039", "https://i.ibb.co/xhfcbgP/images.png"];
+        var shopeepay = ["shopepay-304040", "https://i.ibb.co/j4FWpq2/images.png"];
+        var iSAKU = ["isaku-347161", "https://i.ibb.co/9NKyVj8/images-1.png"];
+        var APIcekId = ["https://api.multipratamaasia.id/", "v2.1", "ewallet", "game", "8dbf10433ec796d"];

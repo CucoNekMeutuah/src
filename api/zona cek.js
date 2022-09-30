3dccff9efc2137a4a33f55d5ace6f2778d9bf37d
Cuco Nek Meutuah <masri18ac@gmail.com>
2022-09-30 08:32:59 +0700

    Create zona cek.js

---
+++ api/zona cek.js
@@ -0,0 +1,23 @@
+ var Game = ["mobilelegends"];
+
+                $('#check-id').click(function (e) {
+             const wrapHasilUsername = document.querySelector('.wrap-hasil-username');
+            e.preventDefault();
+            var id = $('#nomor-telepon').val();
+            $.ajax({
+               method: 'GET',
+               url: link + versi + '/' + account + '/' + accountName + '/' + '?hp=' + id  + '&key=' + key,
+               dataType: 'json',
+               data: {
+                  id: id,
+               },
+               beforeSend: function () {
+                  wrapHasilUsername.innerHTML = `<div class="text-center"><div class="spinner-border text-center" role="status"></div>`;
+               },
+               success: function (data) {
+                  wrapHasilUsername.innerHTML = `<input id="hasil-username" type="text" required="required" class="form-control" value="" placeholder="Username Player" disabled>`;
+                  $('#hasil-username').val(data['nickname']);
+                  
+                  },
+            });
+         });

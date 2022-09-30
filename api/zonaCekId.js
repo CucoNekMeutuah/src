695c7c6d627ff1b186d9e8e06d569ec946a2b2cb
Cuco Nek Meutuah <masri18ac@gmail.com>
2022-09-30 10:58:45 +0700

    Update zonaCekId.js

--- api/zonaCekId.js
+++ api/zonaCekId.js
@@ -1,12 +1,15 @@
+			//function cek nama player id game
+
  var Game = ["mobilelegends"];
 
                 $('#check-id').click(function (e) {
              const wrapHasilUsername = document.querySelector('.wrap-hasil-username');
             e.preventDefault();
             var id = $('#nomor-telepon').val();
+            var zoneId = $('#zoneId').val();
             $.ajax({
                method: 'GET',
-               url: link + versi + '/' + account + '/' + accountName + '/' + '?hp=' + id  + '&zone=' + zoneId + '&key=' + key,
+               url: 'https://api.multipratamaasia.id/v2.1/game/mobilelegends/?id=' + id + '&zone=' + zoneId + '&key='+key,
                dataType: 'json',
                data: {
                   id: id,
@@ -15,7 +18,7 @@
                   wrapHasilUsername.innerHTML = `<div class="text-center"><div class="spinner-border text-center" role="status"></div>`;
                },
                success: function (data) {
-                  wrapHasilUsername.innerHTML = `<input id="hasil-username" type="text" required="required" class="form-control" value="" placeholder="Username Player" disabled>`;
+                  wrapHasilUsername.innerHTML = `<input id="hasil-username" type="text" required="required" class="form-control" value="" placeholder="nickname Player" disabled>`;
                   $('#hasil-username').val(data['nickname']);
                   
                   },

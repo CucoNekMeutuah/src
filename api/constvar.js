e2306664100a770afe959d3e2067dec91a290592
Cuco Nek Meutuah <masri18ac@gmail.com>
2022-10-01 16:34:54 +0700

    Create constvar.js

---
+++ api/constvar.js
@@ -0,0 +1,21 @@
+        var g3tApi = ["DonPay", "https://bo.dipayin.my.id/dev/public/v2/api/", "https://bo.dipayin.my.id/dev/public/v2/detail/", "073809c85bbee275e8e77b7969799bde34244ef537dc514252f7d6c3229e7d9c"];
+        
+        var cp = ["https://i.ibb.co/BCbGrkm/20220624-035810.png"];
+        
+        var nama2 = ["DANA", "OVO", "LinkAja", "Grab", "GoPay", "ShopeePay", "iSAKU", "AOV", "FreeFire", "CallOfDuty", "HiggsDomino", "BGMI", "GENSIN", "LORDMOBILE", "MARVELSW", "RAGNAROK", "POINTBLANK"];
+        var aov = ["arenaofvalor-338119", "https://i.ibb.co/H7WtpW2/images-8.jpg"];
+        var freefire = ["freefire-310252", "https://i.ibb.co/KFjtMT7/images-4.jpg"];
+        var callofduty = ["callofduty-313838", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwawNp1YqA6pXotTB8K9KChvsNLfFtUqZCdQ&usqp=CAU"];
+        var higgsdomino = ["higgsdomino-310253", "https://i.ibb.co/9wPy8V1/unnamed.png"];
+        var lmobile = ["lordmobile-347333", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpHyFlzTCr8ikh8ByXCzaS5ij2P1M2HLAwiw&usqp=CAU"];
+        var marvelsw = ["marvelsuperwar-338122", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVuMRswRosCdidx9HPYHlM2s_-6IutcVMVXQ&usqp=CAU"];
+        var pointblank = ["pointblank-338121", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtfSdll0qlFHaKtzG1gKu6pRVbn5FqZFCdGQ&usqp=CAU"];
+
+        var dana = ["dana-304037", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-9p6xMnSxAKgy71KAacDVO2b5D7IeaaSziA&usqp=CAU"];
+        var ovo = ["ovo-304038", "https://i.ibb.co/TTHhhNG/20220624-025928.png"];
+        var linkaja = ["linkaja-345950", "https://i.ibb.co/HXy9LmG/images-1.png"];
+        var grab = ["grab-347162", "https://i.ibb.co/RQHcnxn/images-2.png"];
+        var gopay = ["gopay-304039", "https://i.ibb.co/xhfcbgP/images.png"];
+        var shopeepay = ["shopepay-304040", "https://i.ibb.co/j4FWpq2/images.png"];
+        var isaku = ["isaku-347161", "https://i.ibb.co/9NKyVj8/images-1.png"];
+        var APIcekId = ["https://api.multipratamaasia.id/", "v2.1", "ewallet", "game", "8dbf10433ec796d"];
